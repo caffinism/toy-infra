77e65e73184024336c3b3665fc1b2ca80483a656
scott <geniuspark@gmail.com>
2023-01-04 10:38:03 +0900

    refact

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 # need to manage nameservers for ${var.domain} from freenom.com
 output "name_servers"{
   value = aws_route53_zone.domain.name_servers
-  description = "need to manage nameservers for ${var.domain} from freenom.com"
+  description = "need to manage nameservers for var.domain from freenom.com"
 }
