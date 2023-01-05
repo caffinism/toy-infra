ebf11e5a5adfcb526deef986976983636dad7f70
scott <geniuspark@gmail.com>
2023-01-05 16:40:58 +0900

    add cloudfront

--- route53.tf
+++ route53.tf
@@ -26,9 +26,9 @@ resource "aws_route53_record" "jenkins_domain" {
   }
 }
 
-resource "aws_route53_record" "cert" {
-  zone_id = aws_route53_zone.domain.zone_id
-  name    = "${aws_acm_certificate.cert.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.resource_record_type}"
-  records = ["${aws_acm_certificate.cert.resource_record_value}", ]
-}
+# resource "aws_route53_record" "cert" {
+#   zone_id = aws_route53_zone.domain.zone_id
+#   name    = "${aws_acm_certificate.cert.resource_record_name}"
+#   type    = "${aws_acm_certificate.cert.resource_record_type}"
+#   records = ["${aws_acm_certificate.cert.resource_record_value}", ]
+# }
