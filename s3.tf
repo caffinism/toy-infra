ef92cb3901f300b20e6d3be2f1d45ab7d1bb3bfb
scott <geniuspark@gmail.com>
2023-01-05 14:38:40 +0900

    add s3

--- s3.tf
+++ s3.tf
@@ -36,3 +36,11 @@ resource "aws_s3_bucket_public_access_block" "fe" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_website_configuration" "fe" {
+  bucket = aws_s3_bucket.fe.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+}
