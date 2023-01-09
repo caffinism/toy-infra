a4a2d492be06870ad0295667b2769ca507e5d7be
scott <geniuspark@gmail.com>
2023-01-09 09:37:46 +0900

    add cert

--- acm.tf
+++ acm.tf
@@ -1,8 +1,8 @@
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = "${var.domain}"
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "${var.domain}"
+  validation_method = "DNS"
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
