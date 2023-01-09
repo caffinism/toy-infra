c1a000c2a50eebee7c2cdf4ebeca0dbe52ac49d4
scott <geniuspark@gmail.com>
2023-01-09 09:55:22 +0900

    add cert val op

--- acm.tf
+++ acm.tf
@@ -5,4 +5,9 @@ resource "aws_acm_certificate" "cert" {
   lifecycle {
     create_before_destroy = true
   }
+
+  validation_option {
+    domain_name       = "${var.domain}"
+    validation_domain = "${var.domain}"
+  }
 }
